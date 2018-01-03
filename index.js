6607fba7f80e5a07a12ac9b5825fd57e496888e1
Jérémy Buget <jbuget@gmail.com>
2018-01-03 21:59:57 +0100

    Add prismic libs to vendor tree

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /* eslint-env node */
 'use strict';
+const map = require('broccoli-stew').map;
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-prismic',
@@ -14,4 +17,13 @@ module.exports = {
     target.import('vendor/shims/prismic-dom.js');
   },
 
+  treeForVendor(defaultTree) {
+    let prismicJSLib = new Funnel('node_modules/prismic-javascript/dist/prismic-javascript.min.js');
+    prismicJSLib = map(prismicJSLib, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+    let prismicDOMLib = new Funnel('node_modules/prismic-dom/dist/prismic-dom.min.js');
+    prismicDOMLib = map(prismicDOMLib, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+    return new mergeTrees([defaultTree, prismicJSLib, prismicDOMLib]);
+  }
 };
